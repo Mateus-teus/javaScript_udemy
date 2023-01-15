b520f0647d2fb918e5bdb794b5afd700c41c4868
Mateus-teus <114873188+Mateus-teus@users.noreply.github.com>
2023-01-14 23:06:43 -0300

    Update script.js

--- funções/função5/script.js
+++ funções/função5/script.js
@@ -1,11 +1,17 @@
-let y = 5;
-	const multiplicar = function(n){
-	let y =  n * 2;
-	console.log(y) //escopo função
-	if(y == 10){
-	let y = 55;
-	console.log(y) //escopo if dentro da função
+let a = 10;
 
+function multiplicar(x,y){
+	let a = x * y;
+
+	if(a > 10){
+		let a = 0
+		a++
+		console.log(a); // este é da condição if
+	}
+
+	console.log(a); // este é da função
 }
-}
-	multiplicar(10); 
+
+console.log(a); // este é o escopo global (10)
+
+multiplicar(1,7); // aqui chama a função e passa os parametros
