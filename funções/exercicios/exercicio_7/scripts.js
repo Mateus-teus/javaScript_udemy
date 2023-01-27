bab29fe84e4d84da25808fde8901fd8fb26c8c07
Mateus-teus <114873188+Mateus-teus@users.noreply.github.com>
2023-01-26 22:39:12 -0300

    exercicios

---
+++ funções/exercicios/exercicio_7/scripts.js
@@ -0,0 +1,7 @@
+function pot(a,b){
+    //return x = Math.pow(a,b);
+    return a**b
+    
+}
+
+console.log(pot(9,3))
---
+++ funções/exercicios/exercicio_8/scripts.js
@@ -0,0 +1,12 @@
+function num(n){}
+    while (n >=0){
+    n--
+   if(n % 2== 1){
+    continue;
+   }
+
+   n = n
+   
+}
+
+console.log(num(25))
